89a6cf26aa4cb90db66aabeaab45c50f92bff392
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-04-26 15:07:13 -0300

    create signOut context

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -13,7 +13,7 @@ import Logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import {useAuth} from '../../context/AuthContext';
+import {useAuth} from '../../hooks/AuthContext';
 
 interface SignInFormData {
   email: string;
