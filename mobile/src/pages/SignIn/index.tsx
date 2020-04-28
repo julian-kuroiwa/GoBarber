1c25a7f963332e2c667133da7fb0c0aec02888d4
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-04-28 12:07:27 -0300

    input and button components

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 import logo from '../../assets/logo.png';
@@ -10,6 +13,9 @@ const SignUp: React.FC = () => {
     <Container>
       <Image source={logo} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => console.log('click')}>Entrar</Button>
     </Container>
   );
 };
