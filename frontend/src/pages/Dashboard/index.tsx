5ce634bf12f1cdf3c6fb1d5320ee3f16947c4504
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-06-03 10:21:34 -0300

    finishing layout dashboard [frontend]

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import React, {useState, useCallback} from 'react';
+import { FiPower, FiClock } from 'react-icons/fi';
+import DayPicker, {DayModifiers} from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
-import {Container, Header, HeaderContainer, Profile} from './styles';
+import {Container, Header, HeaderContainer, Profile, Content, Schedule, Calendar, NextAppointment, Section, Appointment} from './styles';
 
 import LogoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/Auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const {signOut, user} = useAuth();
+
+  const handleDayChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if(modifiers.avaliable) {
+      setSelectedDate(day);
+    }
+  }, [])
 
   return (
     <Container>
@@ -28,6 +37,71 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContainer>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="" alt=""/>
+              <strong>Name</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="" alt=""/>
+                <strong>Name</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+          </Section>
+        </Schedule>
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Outubro',
+              'Novembro',
+              'Dezembro'
+            ]}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0,6]}]}
+            modifiers={{
+              avaliable: {daysOfWeek: [1,2,3,4,5]}
+            }}
+            onDayClick={handleDayChange}
+            selectedDays={selectedDate}
+          />
+        </Calendar>
+      </Content>
     </Container>
   )
 };
