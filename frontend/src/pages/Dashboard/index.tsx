475b2fd802096a987ea62abf069ad7bf09cf2a28
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-06-03 09:07:25 -0300

    finishing header [frontend]

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => (
-  <h1>Dashboard</h1>
-);
+import {Container, Header, HeaderContainer, Profile} from './styles';
+
+import LogoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/Auth';
+
+const Dashboard: React.FC = () => {
+  const {signOut, user} = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContainer>
+          <img src={LogoImg} alt="GoBarber"/>
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContainer>
+      </Header>
+    </Container>
+  )
+};
 
 export default Dashboard;
